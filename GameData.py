f2bf4a1d5a8c413d1cb5cced8ec6b6dc5c0acc1d
Chuffman-CSD <chuffman@stcharlessd.org>
2020-01-27 11:39:01 -0600

    new file:   Database.pickle 	new file:   GameData.py

---
+++ GameData.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#/Chris Huffman
+#1/27/2020
+
+def mainMenu():
+    print("""
+========================
+    GameLibrary .Inc
+========================
+
+---MAIN MANU---
+1.) Add/Edit Game
+2.) Print All Games
+3.) Search Game By Title
+4.) Remove a Game
+5.) Save Database
+
+Q) Quit""")
+
+if __name__ == "__main__":
+    mainMenu()
