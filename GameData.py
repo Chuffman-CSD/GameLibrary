f158b5bfea82d9621d1b5aaaf3a3b5f5e35ee4d6
Chuffman-CSD <chuffman@stcharlessd.org>
2020-01-27 11:55:10 -0600

    modified:   GameData.py

--- GameData.py
+++ GameData.py
@@ -2,6 +2,30 @@
 #/Chris Huffman
 #1/27/2020
 
+def add_edit_game():
+    print("\nRunning: add_edit_game()\n")
+    mainMenu()
+
+def print_all_games():
+    print("\nRunning: print_all_games()\n")
+    mainMenu()
+
+def search_by_title():
+    print("\nRunning: serch_by_title(()\n")
+    mainMenu()
+
+def remove_a_game():
+    print("\nRunning: remove_a_game()\n")
+    mainMenu()
+
+def save_database():
+    print("\nRunning: save_database()\n")
+    mainMenu()
+
+def quit_program():
+    print("\nRunning: quit_program()\n")
+    mainMenu()
+
 def mainMenu():
     print("""
 ========================
@@ -15,7 +39,29 @@ def mainMenu():
 4.) Remove a Game
 5.) Save Database
 
-Q) Quit""")
-
+Q) Quit\n""")
+    x = 0
+    while x == 0:
+        choice = input("Select a numerical option: ")
+        if choice.isdigit():
+            if choice == "1":
+                x = 1
+                print_all_games()
+            elif choice == "2":
+                x = 1
+                print_all_games()
+            elif choice == "3":
+                x = 1
+                search_by_title()
+            elif choice == "4":
+                x = 1
+                remove_a_game()
+            elif choice == "5":
+                x = 1
+                quit_program()
+            else:
+                print("\n")
+        elif choice.isalpha():
+            print("\nAlphabetical Responses are prohibited.\n")
 if __name__ == "__main__":
     mainMenu()
