735b7480cd30a2dbcefb62ebc1c082d5247330cc
Chuffman-CSD <chuffman@stcharlessd.org>
2020-02-04 11:40:20 -0600

    modified:   Database.pickle 	modified:   GameData.py

--- GameData.py
+++ GameData.py
@@ -13,6 +13,9 @@ game_data = open("Database.pickle","rb")
 games = pickle.load(game_data)
 game_data.close()
 
+saved = 0
+changed = 0
+
 def add_edit_game():
     global games
     global key
@@ -78,46 +81,68 @@ Select an option to edit
                 if choice == "1":
                     genre = input("Enter the genre: ")
                     print("Genre changed from",games[key][0],"to",genre,".")
+                    changed += 1
+                    saved = 0
                     games[key][0] = genre
                 if choice == "2":
                     title = input("Enter the title: ")
                     print("Title changed from",games[key][1],"to",title,".")
+                    changed += 1
+                    saved = 0
                     games[key][1] = title
                 if choice == "3":
                     developer = input("Enter the developer: ")
                     print("Developer changed from",games[key][2],"to",developer,".")
+                    changed += 1
+                    saved = 0
                     games[key][2] = developer
                 if choice == "4":
                     publisher = input("Enter the publisher: ")
                     print("Publisher changed from",games[key][3],"to",publisher,".")
+                    changed += 1
+                    saved = 0
                     games[key][3] = publisher
                 if choice == "5":
                     console = input("Enter the console: ")
                     print("Console changed from",games[key][4],"to",console,".")
+                    changed += 1
+                    saved = 0
                     games[key][4] = console
                 if choice == "6":
                     release_year = input("Enter the release year: ")
                     print("Release year changed from",games[key][5],"to",release_year,".")
+                    changed += 1
+                    saved = 0
                     games[key][5] = release_year
                 if choice == "7":
                     personal_rating = input("Enter the personal rating: ")
                     print("Personal rating changed from",games[key][6],"to",personal_rating,".")
+                    changed += 1
+                    saved = 0
                     games[key][6] = personal_rating
                 if choice == "8":
                     compatibility = input("Enter the player compatibility: ")
                     print("compatibility changed from",games[key][7],"to",compatibility,".")
+                    changed += 1
+                    saved = 0
                     games[key][7] = compatibility
                 if choice == "9":
                     price = input("Enter the player compatibility: ")
                     print("Price changed from",games[key][8],"to",price,".")
+                    changed += 1
+                    saved = 0
                     games[key][8] = price
                 if choice == "10":
                     progress = input("Enter the player compatibility: ")
                     print("Beat it changed from",games[key][9],"to",progress,".")
+                    changed += 1
+                    saved = 0
                     games[key][9] = progress
                 if choice == "11":
                     year_bought = input("Enter the player compatibility: ")
                     print("Purchase year changed from",games[key][9],"to",year_bought,".")
+                    changed += 1
+                    saved = 0
                     games[key][9] = year_bought
                 if choice == "12":
                     genre = input("Enter the genre: ")
@@ -152,13 +177,15 @@ Select an option to edit
                     games[key][9] = progress
                     year_bought = input("Enter the player compatibility: ")
                     print("\nPurchase year changed from",games[key][9],"to",year_bought,".")
+                    changed += 1
+                    saved = 0
                     games[key][9] = year_bought
                 if choice == "13":
-                    pass
+                    print("\n***Cancelling!***\n")
             elif choice.isalpha():
-                pass
+                print("\n***Invalid response. [",choice,"]***")
     elif choice in unknown_entries_special:
-        pass
+        print("\n***Invalid character. [",choice,"]***")
     elif choice == "3":
         print("\nLoading main manu!\n")
 
@@ -185,7 +212,8 @@ def print_all_games():
             print("| Did you beat the game? [Y]/[N]: ",games[key][9])
             print("| Year Bought: ",games[key][10])
             print("----------")
-        print("There are",key,"games in the database!")
+        key = len(games)
+        print("\nThere are",key,"games in the database!\n")
 
 def search_by_title():
     tally = 0
@@ -199,16 +227,59 @@ def search_by_title():
    
 
 def remove_a_game():
-    print("\nRunning: remove_a_game()\n")
-
+    global key
+    global saved
+    global changed
+    for key in games.keys():
+        print(key)
+        print("\n",key,"-",games[key][1],"\n")
+        if key == 0:
+            print("\n***There is no game data.***")
+    if games == {}:
+        print("\n***There is no game data.***")
+    else:
+        edit_key = int(input("Which game would you like to remove: "))
+        if edit_key in games:
+            print("\nRemoving: ",games[edit_key][1],"from the database..\n")
+            print("***",games[edit_key][1],"has been removed!***")
+            games.pop(edit_key)
+            changed += 1
+            saved = 0
+            key = len(games)
+            print("\nThere are now",key,"games in the database!\n")
+            
+        elif edit_key > len(games):
+            print("\n",edit_key,"is not a valid option!\n")
+            edit_key = None    
+            if edit_key == None:
+                edit_key = 0
+                print("\n")
+            
+        else:
+            print("\n",edit_key,"is not a valid option!\n")
+            edit_key = None    
+            if edit_key == None:
+                edit_key = 0
+                print("\n")
+    
 def save_database():
-    print("\nRunning: save_database()\n")
+    saved += 1
+    print("\n***Successfully saved the current database!***\n")
     game_data = open("Database.pickle","wb")
     pickle.dump(games,game_data)
     game_data.close()
+    if saved == 0 and changed == 0:
+        saved = 0
+        changed = 0
+    elif saved == 0 and changed >= 1:
+        saved = 1
+        changed = 0
 
 def mainMenu():
-    global unkown_entries
+    global changed
+    global games
+    global saved
+    global unknown_entries_special
     while True:
         print("""
 ========================
@@ -222,7 +293,7 @@ def mainMenu():
 | 4.) Remove a Game
 | 5.) Save Database
 
-| 6) Quit\n""")
+| 6.) Quit\n""")
         choice = input("Select a numerical option: ").lower()
         if choice.isdigit():
             if choice == "1":
@@ -234,11 +305,38 @@ def mainMenu():
             elif choice == "4":
                 remove_a_game()
             elif choice == "5":
-                save_database()
+                if saved == 0:
+                    print("\n***The current database has no changes to be saved.***\n")
+                    choice = input("Save anyways? [Y]/[N]: ").upper()
+                    if choice == "Y":
+                        print("\n*Database Saved.*\n")
+                    elif choice == "N":
+                        print("\n*Loading Main Menu.*\n")
+                else:
+                    save_database()
             elif choice == "6":
-                exit()
-                games = pickle.dump(game_data)
-                game_data.close()
+                if saved == 0 and changed == 0:
+                    print("\nGoodbye!\n")
+                    exit()
+                elif saved == 0 and changed >= 1:
+                    x = 0
+                    while x == 0:
+                        choice = input("Woah there, you never saved! Do you wish to save? [Y]/[N]: ").upper()
+                        if choice == "Y":
+                            x = 1
+                            data = open("Database.pickle","wb")
+                            pickle.dump(games,data)
+                            data.close()
+                            print("\n***Saved!***\n")
+                        elif choice == "N":
+                            x = 1
+                            print("\n***Not saved!***\n")
+                            exit()
+                        else:
+                            print("\n***",choice,"is not a valid response!***\n")
+                elif saved == 1 and changed >= 1:
+                    print("\nGoodbye!\n")
+                    exit()
         elif choice.isalpha():
             print("\n***Invalid character. [",choice,"]***")
         elif choice in unknown_entries_special:
